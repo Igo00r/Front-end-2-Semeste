7ac9a7d9f8c983634991c4a718fc24c9db1eba8f
Igoorr <aigor9573@gmail.com>
2025-02-26 12:20:15 -0300

    Add files via upload

---
+++ aula03/src/app/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Botao from "./components/Botao/Botao";
+import Card, { Auxiliar } from "./components/Card/page";
+import Meucomponente from "./components/Meucomponente/page";
+import ToDo from "./components/ToDo/ToDo";
+
+export default function Home() {
+  return (
+    <>
+    
+      <h1>Hello world!!!</h1>
+      
+      <Card />
+
+      <Meucomponente />
+
+      <Auxiliar/>
+
+      <Botao cor='red' texto='Excluir' clicado={()=>{ alert("Botão excluir foi pressionado!") }} />
+
+      <Botao cor='green' texto='Acessar' />
+      
+      <Botao texto='Confirmar' clicado={() =>{ alert("Botão Confirmar foi pressionado!")}}/>
+      
+      <ToDo />
+
+
+    </>
+  )
+}
